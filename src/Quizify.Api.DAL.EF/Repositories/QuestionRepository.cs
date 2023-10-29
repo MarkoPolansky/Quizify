ae8a386480eca1458bf3e6c22eccb9bb327413a4
cibetkaluk <79983149+cibetkaluk@users.noreply.github.com>
2023-10-29 14:10:49 +0100

    Added question repository and tests.

--- src/Quizify.Api.DAL.EF/Repositories/QuestionRepository.cs
+++ src/Quizify.Api.DAL.EF/Repositories/QuestionRepository.cs
@@ -1,4 +1,6 @@
-﻿using Quizify.Api.DAL.EF.Entities;
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Quizify.Api.DAL.EF.Entities;
 using Quizify.Api.DAL.EF.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -11,9 +13,48 @@ namespace Quizify.Api.DAL.EF.Repositories
 {
     public class QuestionRepository : RepositoryBase<QuestionEntity>, IQuestionRepository
     {
-        public QuestionRepository(QuizifyDbContext dbContext) : base(dbContext)
+        private readonly IMapper _mapper;
+        public QuestionRepository(QuizifyDbContext dbContext, IMapper mapper) : base(dbContext)
         {
+            _mapper = mapper;
         }
-        
+
+        public override QuestionEntity? GetById(Guid id)
+        {
+            return dbContext.Set<QuestionEntity>()
+                .Include(question => question.ActiveInQuiz)
+                .Include(question => question.Answers)
+                .Include(question => question.Quiz)
+                .SingleOrDefault(entity => entity.Id == id);
+        }
+
+        public override Guid? Update(QuestionEntity question)
+        {
+            if (Exists(question.Id))
+            {
+                var existingQuestion = dbContext.Questions
+                    .Include(question => question.ActiveInQuiz)
+                    .Include(question => question.Answers)
+                    .Include(question => question.Quiz)
+                    .Single(r => r.Id == question.Id);
+
+                _mapper.Map(question, existingQuestion);
+
+                foreach (var AnswerEntity in existingQuestion.Answers)
+                {
+                    dbContext.Answers.Add(AnswerEntity);
+                }
+
+                dbContext.Questions.Update(existingQuestion);
+                dbContext.SaveChanges();
+
+                return existingQuestion.Id;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
     }
 }
