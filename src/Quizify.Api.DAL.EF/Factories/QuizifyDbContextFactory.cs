6037e0707bacfcc0e55ae9a1cbf65f9244864098
cibetkaluk <79983149+cibetkaluk@users.noreply.github.com>
2023-12-12 10:46:25 +0100

    Bad use of constring for testing

--- src/Quizify.Api.DAL.EF/Factories/QuizifyDbContextFactory.cs
+++ src/Quizify.Api.DAL.EF/Factories/QuizifyDbContextFactory.cs
@@ -16,7 +16,7 @@ namespace Quizify.Api.DAL.EF.Factories
             var optionsBuilder = new DbContextOptionsBuilder<QuizifyDbContext>();
 
             //optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
-            optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = quizify; Integrated Security = True");
+            optionsBuilder.UseSqlServer("Server=tcp:sqldb-iw5-2023-team-xletak00.database.windows.net,1433;Initial Catalog=db-iw5-2023-team-xletak00;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;Authentication=\"Active Directory Default\";");
             return new QuizifyDbContext(optionsBuilder.Options);
         }
     
